acc089e507e1ee3a29f30333c84a946d6982decc
paleto30 <paleto0817@gmail.com>
2023-04-14 09:28:12 -0500

    feat(falto la mitad) : :rocket: se hizo lo que se pudo

---
+++ js/db.js
@@ -0,0 +1,125 @@
+
+export default{
+
+
+   home:{
+      seccion1:{
+         imgM:"/images/logoWhite.png",
+         menus:[
+            {
+               href:"index.html",
+               text:"Inicio"
+            },
+            {
+               href:"servicios/index.html",
+               text:"Servicios",
+            },
+            {
+               href:"soporte/index.html",
+               text:"Soporte"
+            }
+         ]
+      },
+      seccion2:{
+         img:"/images/Space astronaut cartoon.png",
+         list:[
+            {
+               src:"images/whatsapp.svg",
+               href:"https://web.whatsapp.com/",
+               text:"+57 311 8807659"
+            },
+            {
+               src:"images/twitter.svg",
+               href:"https://twitter.com/?lang=es",
+               text:"@CampusLatam"
+            },
+            {
+               src:"images/facebookF.svg",
+               href:"https://es-la.facebook.com/",
+               text:"@Campusland",
+            },
+            {
+               src:"images/instagram.svg",
+               href:"https://www.instagram.com/",
+               text:"@campusdevelopers"
+            }
+         ]
+      }
+   },
+
+
+    servicio:{
+
+      seccion1:{
+         img:"/images/logoWhite.png",
+         menus:[
+            {
+               href:"../index.html",
+               text:"Inicio"
+            },
+            {
+               href:"../servicios/index.html",
+               text:"Servicios",
+            },
+            {
+               href:"soporte/index.html",
+               text:"Soporte"
+            }
+         ]
+      },
+
+      seccion2:{
+         h1:"Servicios",
+         href:"../tiendaComida/index.html",
+         text:"Cafeteria Virtual"
+      },
+
+      seccion3:[
+         {
+            title: "Hunters",
+            img: "../images/channels4_profile.jpg",
+            btn: "ver datos"
+         },
+
+         {
+            title: "Cafeteria Hunters",
+            img: "../images/channels4_profile.jpg",
+            btn: "ver datos"
+         },
+         {
+            title: "8vo piso",
+            img: "../images/channels4_profile.jpg",
+            btn: "ver datos"
+         },
+         {
+            title: "Baños",
+            img: "../images/channels4_profile.jpg",
+            btn: "ver datos"
+         }
+      ]
+   },
+    cafeteria:[
+         {
+            img:"",
+            title:"",
+            precio:"",
+            btn:""
+         },
+         {
+            img:"",
+            title:"",
+            precio:"",
+            btn:""
+         },
+         {
+            img:"",
+            title:"",
+            precio:"",
+            btn:""
+         }
+
+      ]
+
+
+    
+}
---
+++ servicios/main.js
@@ -0,0 +1,54 @@
+import dinamic from "../js/db.js";
+
+document.addEventListener("DOMContentLoaded",(e)=>{
+   cargarCards();
+   cargarTitle();
+   //cargarMenu();
+})
+
+
+function cargarMenu(){
+
+    const secc1 = document.querySelector("#seccion1");
+    secc1.insertAdjacentHTML("beforeend",
+    `
+        <img id="logo" src="${dinamic.servicio.seccion1.img}" alt="">
+    `)
+    let men = ''
+    dinamic.servicio.seccion1.menus.forEach(el=>{
+        const {href ,text } = el;
+        men+=`
+        <a class="alink  ss" href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>
+        `
+    })
+  
+}
+
+function cargarTitle(){
+
+    const secc = document.querySelector("#seccion2");
+    let html = `
+        <h1>${dinamic.servicio.seccion2.h1}</h1>
+        <p><a href="${dinamic.servicio.seccion2.href}" target="_blank" rel="noopener noreferrer">${dinamic.servicio.seccion2.text}</a></p>
+    `
+    secc.innerHTML = html;
+}
+
+function cargarCards() {
+    console.log(dinamic);
+    let html = ''
+    const card = document.querySelector("#cards");
+    dinamic.servicio.seccion3.forEach(el =>{
+        const {title ,img ,btn} = el;
+        html+=`
+        <div class="tarjetas " id="#contenedor-tarjetas">
+            <h3>${title}</h3>
+           <img src="${img}" alt="" srcset="">
+           <button >${btn}</button>
+        </div>
+        `
+        card.innerHTML = html;
+    });
+}
+
+
